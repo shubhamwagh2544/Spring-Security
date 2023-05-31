2ed7c61d01e57f53e7aaa964aadc6a74f8295ab0
Shubham Wagh <shubhamwagh2544@gmail.com>
2023-05-31 21:37:46 +0530

    password encode

--- src/main/java/com/springboot2/security/security/ApplicationSecurityConfig.java
+++ src/main/java/com/springboot2/security/security/ApplicationSecurityConfig.java
@@ -1,14 +1,26 @@
 package com.springboot2.security.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @Configuration
 @EnableWebSecurity
 public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    private final PasswordEncoder passwordEncoder;
+    @Autowired
+    public ApplicationSecurityConfig(PasswordEncoder passwordEncoder) {
+        this.passwordEncoder = passwordEncoder;
+    }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
@@ -22,4 +34,15 @@ public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
                 .httpBasic();
     }
 
+    @Override
+    @Bean
+    protected UserDetailsService userDetailsService() {
+        UserDetails userJohnWick = User.builder()
+                .username("johnwick")
+                .password(passwordEncoder.encode("johnwick"))
+                .roles("STUDENT")
+                .build();
+
+        return new InMemoryUserDetailsManager(userJohnWick);
+    }
 }
