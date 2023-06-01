9bcc13dd2d394f762182d383536e5d6e0ab0c9e9
Shubham Wagh <shubhamwagh2544@gmail.com>
2023-06-01 08:36:42 +0530

    role based auth

--- src/main/java/com/springboot2/security/security/PasswordConfig.java
+++ src/main/java/com/springboot2/security/security/PasswordConfig.java
@@ -10,7 +10,7 @@ public class PasswordConfig {
 
     @Bean
     public PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
+        return new BCryptPasswordEncoder(10);
     }
 
 }
