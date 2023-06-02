4c111ad2c591bbc0fadf4074967cdbd8a7a40dc5
Shubham Wagh <shubhamwagh2544@gmail.com>
2023-06-02 09:11:13 +0530

    CSRF-TOKEN

--- src/main/java/com/springboot2/security/security/ApplicationSecurityConfig.java
+++ src/main/java/com/springboot2/security/security/ApplicationSecurityConfig.java
@@ -12,6 +12,8 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
+import org.springframework.security.web.csrf.CsrfTokenRepository;
 
 import static com.springboot2.security.security.ApplicationUserRole.*;
 
@@ -29,6 +31,8 @@ public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
+                //.csrf().csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
+                //.and()
                 .csrf().disable()
                 .authorizeRequests()
                 .antMatchers("/", "index", "/css/*", "/js/*").permitAll()
